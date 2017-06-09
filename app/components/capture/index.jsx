8f18d0f9e8d503c2f236e914c9cd9cce4b4d44a2
Rajib Ahmed <rajib.ahmed@klarna.com>
2017-06-09 12:24:15 +0200

    Add onClick to show props.

--- app/components/capture/index.jsx
+++ app/components/capture/index.jsx
@@ -3,10 +3,18 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {RaisedButton} from 'material-ui';
 
 export default class Capture extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	showName() {
+		window.alert(this.props.name);		
+	}
+
   render() {
 		return (
 			<MuiThemeProvider>
-				<RaisedButton label="Default" />
+				<RaisedButton label="Default" onClick={ this.showName.bind(this) } />
 			</MuiThemeProvider>
 		)
   }
