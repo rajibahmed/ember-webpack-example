496e77658691e5867456f3f3a8fc713f886bf384
Rajib Ahmed <rajib.ahmed@valtech.se>
2017-07-19 15:17:59 +0200

    Add editorConfig and indentation fix.

--- app/components/capture/index.jsx
+++ app/components/capture/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Button} from '@klarna/bubble-ui/lib';
+import { Button } from '@klarna/bubble-ui/lib';
 
 export default class Capture extends React.Component {
 	showMessage() {
 		window.alert(this.props.name);
 	}
-  render() {
+	render() {
 		return (
-				<Button.Primary onClick={this.showMessage.bind(this)} >Something</Button.Primary>
+			<Button.Primary onClick={this.showMessage.bind(this)} >Something</Button.Primary>
 		)
-  }
+	}
 }
